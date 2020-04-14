0b5f98c42487a803d70c2fdc8d4a33f7247ed2ab
Shubhangi1.Priya <Shubhangi1.Priya@ril.com>
2020-04-14 13:59:45 +0530

    adding java files

---
+++ main2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main
+{
+	// Read multi-line input from console in Java by using Scanner class
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNextLine()) {
+			String[] tokens = scanner.nextLine().split("\\s");
+			System.out.println(Arrays.toString(tokens));
+		}
+
+		scanner.close();
+	}
+}
